ea5d671027d38ceeccb941d1f9c79bfbfdade22c
Krzysztof Grzybek <krzysztof.grzybek89@gmail.com>
2019-10-03 21:41:48 +0200

    fix y-combinator

--- examples/8-y-combinator.js
+++ examples/8-y-combinator.js
@@ -1,4 +1,4 @@
 const Y = f => (x => f(y => (x(x))(y)))(x => f(y => (x(x))(y)));
 
-const COUNTDOWN = Y(f => i => i >= 0 && (console.log(i), () => f(i - 1)));
+const COUNTDOWN = Y(f => i => i >= 0 && (console.log(i), f(i - 1)));
 COUNTDOWN(10);
